1dfc88adf28326d6888e8c19931f75ff5adcf09c
Ploug <frederik@ploug-johansen.dk>
2015-11-08 03:57:26 +0100

    Tcp framework updated to handle more clients.

--- Ball of Duty Server/Domain/Game.cs
+++ Ball of Duty Server/Domain/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Ball_of_Duty_Server.DAO;
 using Ball_of_Duty_Server.Persistence;
 
 namespace Ball_of_Duty_Server.Domain
@@ -16,7 +17,16 @@ namespace Ball_of_Duty_Server.Domain
         {
             _players.Add(player.Id, player);
             player.CurrentCharacter = Map.AddCharacter(); // TODO data to character creation should be dynamic
-            Map.Broker.AddTarget(player.Id, clientIp, clientPort);
+
+            GameObjectDAO data = new GameObjectDAO();
+            data.X = player.CurrentCharacter.Body.Position.X;
+            data.Y = player.CurrentCharacter.Body.Position.Y;
+            data.Width = player.CurrentCharacter.Body.Width;
+            data.Height = player.CurrentCharacter.Body.Height;
+            data.Id = player.CurrentCharacter.Id;
+
+            Map.Broker.AddPlayer(player.Id, data, clientIp,
+                clientPort);
         }
 
         public void RemovePlayer(int playerId)
